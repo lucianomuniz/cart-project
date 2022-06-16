5812b4c3674844bb3af71dcda1e8ccf9345e3285
Luciano Muniz <luciano.mo@gmail.com>
2022-06-16 11:06:03 -0300

    added reducer to remove item, increase and decrease amount cart

---
+++ src/reducer.js
@@ -0,0 +1,37 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CLEAR_CART':
+      return { ...state, cart: [] };
+    case 'REMOVE':
+      return {
+        ...state,
+        cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+      };
+    case 'INCREASE':
+      let tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload)
+          return { ...cartItem, amount: cartItem.amount + 1 };
+        return cartItem;
+      });
+      return {
+        ...state,
+        cart: tempCart,
+      };
+    case 'DECREASE':
+      let temp = state.cart
+        .map((cartItem) => {
+          if (cartItem.id === action.payload)
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          return cartItem;
+        })
+        .filter((cartItem) => cartItem.amount !== 0);
+      return {
+        ...state,
+        cart: temp,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
